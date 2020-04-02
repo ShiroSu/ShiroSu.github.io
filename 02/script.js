ae120441eb4cf0060a61c69afa7930359d337012
ShiroSu <61061507+ShiroSu@users.noreply.github.com>
2020-04-02 17:06:33 +0400

    Over and over again...

--- 02/script.js
+++ 02/script.js
@@ -5,8 +5,8 @@ window.onload=function() {
   let toggle=true;
   let width=document.body.clientWidth;
   let max_width=window.screen.width;
-  alert(width+"\n"+max_width);
   let contact=document.querySelector(".contact");
+  console.log(width+"\n"+max_width);
   if (width<320) {
     contact.innerHTML="Contacts";
   }
@@ -14,13 +14,10 @@ window.onload=function() {
     toggle=!toggle;
     if (toggle) {
       arrow.classList.add("arrow_flip");
-      arrow.classList.remove("tip");
-      arroWrapper.classList.add("wrapper-move");
       menu.classList.add("menu_active");
     }
     else {
       arrow.classList.remove("arrow_flip");
-      //arrow.classList.add("tip");
       menu.classList.remove("menu_active");
     }
   });
