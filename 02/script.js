e7da2c9152d147365058c67b467d8ad7412aa123
ShiroSu <61061507+ShiroSu@users.noreply.github.com>
2020-04-02 13:17:33 +0400

    Reborn again

---
+++ 02/script.js
@@ -0,0 +1,25 @@
+window.onload=function() {
+  let arrow=document.querySelector(".arrow");
+  let arroWrapper=document.querySelector(".arrow-wrapper");
+  let menu=document.querySelector(".menu");
+  let toggle=true;
+  let width=document.body.clientWidth;
+  let contact=document.querySelector(".contact");
+  if (width<320) {
+    contact.innerHTML="Contacts";
+  }
+  arroWrapper.addEventListener("click", ()=> {
+    toggle=!toggle;
+    if (toggle) {
+      arrow.classList.add("arrow_flip");
+      arrow.classList.remove("tip");
+      arroWrapper.classList.add("wrapper-move");
+      menu.classList.add("menu_active");
+    }
+    else {
+      arrow.classList.remove("arrow_flip");
+      //arrow.classList.add("tip");
+      menu.classList.remove("menu_active");
+    }
+  });
+}
