0e59560f70ca44d8b9e31610efcf5ffc1f30a3ab
ShiroSu <61061507+ShiroSu@users.noreply.github.com>
2020-04-03 04:42:42 +0400

    Second reborn

---
+++ 01/script.js
@@ -0,0 +1,34 @@
+
+window.onload=function() {
+  var ico=document.querySelector(".ico");
+  var icoInner=document.querySelector(".ico-inner");
+  var leftMenu=document.querySelector(".left_menu");
+  var icoInner_after=document.querySelector(".ico-inner:after")
+  var sectionLeft=document.querySelector(".news")
+  var toggle=false;
+  ico.addEventListener("click", (e)=> {
+    if (!toggle) {
+      console.log(e);
+      toggle=true;
+      ico.classList.add("ico_active");
+      ico.classList.remove("animate_ico");
+      icoInner.classList.add("ico-inner_active");
+      leftMenu.classList.add("left_menu_active");
+      icoInner.classList.add("inner-left");
+      sectionLeft.classList.add("news_active");
+      leftMenu.style.animation="none";
+      icoInner_after.style.width="44px";
+    }
+    else {
+      toggle=false;
+      sectionLeft.classList.remove("news_active");
+      icoInner.classList.remove("inner-left");
+      leftMenu.classList.remove("left_menu_active");
+      icoInner.classList.remove("ico-inner_active");
+      ico.classList.remove("ico_active");
+      ico.classList.add("animate_ico");
+      leftMenu.style.animation="menu_drag 1.5s infinite forwards";
+      icoInner_after.style.width="0px";
+    }
+  });
+}
