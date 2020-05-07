89b5ec29de2dbba2ae9223f20eef97af0a943f35
ShiroSu <61061507+ShiroSu@users.noreply.github.com>
2020-05-07 13:31:20 +0400

    page

--- script.js
+++ script.js
@@ -1,31 +1,41 @@
-let width=window.innerWidth, height=window.innerHeight,
-    preload=document.querySelector(".preload");
-    preText=document.querySelector(".pre-text");
+let width=window.innerWidth, height=window.innerHeight;
 
 function init() {
+    let preload=document.querySelector(".preload"),
+    preText=document.querySelector(".pre-text"),
+    sections=document.querySelectorAll("section"),
+    footer=document.querySelector("footer");
+    
+    window.scrollTo(0,0);
     anime({
         targets: preText,
         rotate: { 
-            value: 90,
+            value: -90,
             easing: "easeOutElastic",
             delay: 1000,
             duration: 1500,
         },
         translateX: {
-            value: 2*height/3,
+            value: -2*height/3,
+            easing: "easeOutQuad",
             delay: 2700,
-            duration: 1200
+            duration: 600
+        },
+        rotateY: {
+            value: 25,
+            easing: "easeOutQuad",
+            delay:2700,
+            duration: 700
         }
     });
-    setTimeout(()=> preText.style.display="none", 3000);
     anime({
         targets: preload,
         rotateX: 102,
         easing: "easeOutBounce",
-        delay: 4000,
+        delay: 4200,
         duration: 2200
     });
-    setTimeout(()=> preload.style.display="none", 6500);
+    setTimeout(()=> { document.querySelector("body").style.overflowY="overlay"; preload.style.display="none" }, 6500);
     anime({
         targets: ".page",
         translateZ: [-580,0],
@@ -33,6 +43,10 @@ function init() {
         delay: 6800,
         duration: 1000
     });
+    setTimeout(()=> { sections.forEach(item => item.style.display="block"); footer.style.display="block"; }, 7800);
 }
 
 window.onload=init;
+// preload.style.display="none";
+// document.querySelector("body").style.overflowY="auto";
+// document.querySelector(".page").style.transform="none";
