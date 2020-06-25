c16ff4fe2573055477e7ab99d9eb746040656f3a
ShiroSu <61061507+ShiroSu@users.noreply.github.com>
2020-06-26 01:46:25 +0400

    face

--- map/script.js
+++ map/script.js
@@ -19,8 +19,8 @@ dots.forEach(item=> {
         item.style.transitionDuration="0.3s";
         popap.innerHTML=item.getAttribute("id")+": "+item.getAttribute("data-x")+";   "+item.getAttribute("data-y");
         popap.style.opacity=1;
-        popap.style.marginLeft=`${e.clientX+10}px`;
-        popap.style.marginTop=`${e.clientY+10}px`;
+        popap.style.marginLeft=`${e.pageX+10}px`;
+        popap.style.marginTop=`${e.pageY+10}px`;
     });
     item.addEventListener("mouseout", ()=> {
         item.setAttribute("r", "5");
@@ -29,24 +29,46 @@ dots.forEach(item=> {
 });
 azz.forEach(item=> {
     item.addEventListener("mouseenter", (e)=> {
-        item.style.fill="green";
+        item.style.fill="#ffe140";
         popap.innerHTML = item.getAttribute("name");
         popap.style.opacity="1";
-        popap.style.marginLeft=`${e.clientX+10}px`;
-        popap.style.marginTop=`${e.clientY+10}px`;
-        // console.log(`${e.clientX}`);
+        popap.style.marginLeft=`${e.pageX+10}px`;
+        popap.style.marginTop=`${e.pageY+10}px`;
+        // console.log(`${e.pageX}`);
     });
     item.addEventListener("mousemove", e=> {
-        popap.style.marginLeft=`${e.clientX+10}px`;
-        popap.style.marginTop=`${e.clientY+10}px`;
+        popap.style.marginLeft=`${e.pageX+10}px`;
+        popap.style.marginTop=`${e.pageY+10}px`;
     });
     item.addEventListener("mouseout", ()=> {
         popap.style.opacity="0";
-        item.style.fill="#7c7c7c";
+        popap.style.marginLeft=`${0}px`;
+        popap.style.marginTop=`${0}px`;
+        item.style.fill="#bcbcbc";
         // console.clear();
     });
 });
-mapal.addEventListener("click", e=> {
-
-    console.log(e.clientY+"с.ш. "+e.clientX+"в.д.");
-})
+let zoom=document.querySelector(".zoom"), mw,
+zoom_pl=document.querySelector(".enlarge"),
+zoom_mi=document.querySelector(".diminish");
+mw=Number(mapal.width.animVal.valueAsString.replace("%", ""));
+zoom.addEventListener("click", e=>{
+    switch (e.target.id) {
+        case "+": if (mw<100) mw+=5; break;
+        case "-": if (mw>40.1) mw-=5; break;
+    }
+    if (mw>=100) {
+        zoom_pl.style.cursor="default";
+    }
+    else {
+        zoom_pl.style.cursor="pointer";
+    }
+    if (mw<=40.1) {
+        zoom_mi.style.cursor="default";
+    }
+    else {
+        zoom_mi.style.cursor="pointer";
+    }
+    console.log(mw);
+    mapal.setAttribute("width", `${mw}%`);
+});
